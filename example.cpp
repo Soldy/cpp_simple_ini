b821c16bcd308fc85857d2a47aa504b6ff2f1872
Soldy <4786022+Soldy@users.noreply.github.com>
2023-01-20 11:19:59 +0000

    just to for loop
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- example.cpp
+++ example.cpp
@@ -5,8 +5,9 @@
 
 int main(){
     std::map<std::string, std::string> ini = simple_ini_tool::read("example.ini");
-    std::cout << "test_string = " << ini["test_string"] << std::endl;
-    std::cout << "test_number = " << ini["test_number"] << std::endl;
+    for(auto const & [k, v] : ini){
+        std::cout << k << " = " << v << std::endl;
+    }
     return 0;
 }
 
